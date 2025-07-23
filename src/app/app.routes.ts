4d5a30e941795d05881b37a311a667dea5c08365
kauatwn <kaualopescontato@gmail.com>
2025-07-23 20:19:34 -0300

    feat(router): add initial app routes with auth and no-auth guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth/auth.guard';
+import { noAuthGuard } from './core/guards/no-auth/no-auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/login/login.component').then((m) => m.LoginComponent),
+    canActivate: [noAuthGuard],
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
+    canActivate: [noAuthGuard],
+  },
+  {
+    path: 'user-profile',
+    loadComponent: () =>
+      import('./features/user-profile/user-profile.component').then(
+        (m) => m.UserProfileComponent,
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent,
+      ),
+  },
+];
