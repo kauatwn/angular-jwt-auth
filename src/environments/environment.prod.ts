b43ce4668fa11082b73c2e5427d4398c535e1fa5
kauatwn <kaualopescontato@gmail.com>
2025-09-02 16:29:58 -0300

    chore(environment): configure API URLs for Docker and local development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment } from '../app/core/models/environment.model';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: '/api', // Caminho relativo para o proxy
   tokenRefreshBuffer: 30,
   storagePrefix: 'angular_jwt_auth_',
   storage: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { Environment } from '../app/core/models/environment.model';
 
 export const environment: Environment = {
   production: false,
-  apiUrl: '',
+  apiUrl: '', // Defina a URL completa da API para desenvolvimento. Ex.: 'http://localhost:5000', 'http://localhost:8080', etc.
   tokenRefreshBuffer: 30,
   storagePrefix: 'angular_jwt_auth_',
   storage: {
