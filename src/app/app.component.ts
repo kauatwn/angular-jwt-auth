6ac23c217673448ef522b9bed04b0a1bce3ada96
kauatwn <kaualopescontato@gmail.com>
2025-09-03 09:45:32 -0300

    fix: correct typo in styles property in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  sytles: ``,
+  styles: ``,
 })
 export class AppComponent {
   title = 'angular-jwt-auth';
